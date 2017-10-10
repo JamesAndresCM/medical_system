b404f383e583741e3255f74475c7eb5588279097
JamesACM <jaimecanalesmarin@gmail.com>
2017-10-09 22:58:54 -0300

    relacion has_one en modelo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  #callback para que se inserte el id del user al crearse en la tabla role
+  before_create :set_default_role
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -11,7 +13,11 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments
-  
+
+  #un usuario puede tener un solo rol
+  has_one :role
+
+  #validaciones para cover y avatar
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
@@ -19,4 +25,9 @@ class User < ApplicationRecord
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
 
 
+ #se inserta el id del usuario al ser este creado
+  private
+  def set_default_role
+    self.role ||= Role.create()
+  end
 end
