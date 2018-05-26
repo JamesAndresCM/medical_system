47c772815f6fc335b1464f7ffad00ba3fec0310e
JamesACM <jaimecanalesmarin@gmail.com>
2018-05-26 00:43:14 -0400

    se cambia modelo bd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009235714) do
+ActiveRecord::Schema.define(version: 20180525231848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,14 +39,7 @@ ActiveRecord::Schema.define(version: 20171009235714) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.string "cover_file_name"
-    t.string "cover_content_type"
-    t.integer "cover_file_size"
-    t.datetime "cover_updated_at"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
