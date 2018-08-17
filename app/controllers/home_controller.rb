863b4f80d550c7bbad41a8ddb3bceb69f8c4fd02
JamesACM <jaimecanalesmarin@gmail.com>
2018-08-17 01:03:08 -0300

    redirect admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
   before_action :authenticate!
 
   def index
+    redirect_to("/admin/users") if current_user.admin?
     @user_index = Appointment.three_doctor
-    @user_appointment_today = Appointment.get_type_user("user_id", current_user.id).user_appointment_data.week_appointments
+    @user_appointment_today = Appointment.get_type_user("user_id", current_user.id).user_appointment_data.week_appointments.ordenados
   end
 
   def unregistered; end
